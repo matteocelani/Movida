c23819581b8e7e01dbdceaa61a30d494316bb88e
Matteo Celani <matteocelani4@gmail.com>
2020-10-07 18:14:26 +0200

    Configurazione

--- code/src/movida/dalessandrocelani/MovidaCore.java
+++ code/src/movida/dalessandrocelani/MovidaCore.java
@@ -1,7 +1,17 @@
 package movida.dalessandrocelani;
 import movida.commons.*;
 
+import java.io.File;
+import java.util.*;
+
 public class MovidaCore {
+
+    public void loadFromFile(File f) {
+
+    }
+    public void saveToFile(File f) {
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
